c91049774ccedb8b5843fddb164d1429affda417
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-05-16 23:24:29 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')) as f:
+    requires = f.read().splitlines()
+
+setup(
+    author='Ryan Horiguchi',
+    author_email='ryan.horiguchi@gmail.com',
+    dependency_links=[],
+    install_requires=requires,
+    license='unlicense',
+    name='tv_time_export',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    url='git@github.com:rhoriguchi/tv_time_export.git',
+    version='1.0.1',
+    zip_safe=False,
+)
