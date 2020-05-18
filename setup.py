260792875a30492ad81ee121e1521fc6f678cd2a
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-05-19 01:30:14 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     packages=find_packages(),
     python_requires='>=3.7',
     url='git@github.com:rhoriguchi/tv_time_export.git',
-    version='1.0.4',
+    version='1.0.5',
     zip_safe=False,
 )
