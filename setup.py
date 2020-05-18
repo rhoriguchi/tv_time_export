c21ecf8ab5c148b80be234e16c4671090d3f5819
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-05-19 01:28:20 +0200

    Remove dummy file

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')) as f:
-    requires = f.read().splitlines()
 
 setup(
     author='Ryan Horiguchi',
@@ -14,12 +9,16 @@ setup(
             'tv_time_export = tv_time_export.main:main',
         ],
     },
-    install_requires=requires,
+    install_requires=[
+        'beautifulsoup4==4.9.0',
+        'PyYAML==5.3.1',
+        'requests==2.23.0'
+    ],
     license='MIT',
     name='tv_time_export',
     packages=find_packages(),
     python_requires='>=3.7',
     url='git@github.com:rhoriguchi/tv_time_export.git',
-
+    version='1.0.4',
     zip_safe=False,
 )
