1184f2d84c6e435381122e9a688de4882c35bda0
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-08-11 00:12:40 +0200

    Switch to pathlib

--- tv_time_export/tv_time_extractor.py
+++ tv_time_export/tv_time_extractor.py
@@ -1,8 +1,8 @@
 import json
 import logging
-import os
 import sys
 from datetime import datetime
+from pathlib import Path
 
 import yaml
 
@@ -27,10 +27,10 @@ class TvTimeExtractor(object):
         return tv_show_states
 
     def save_tv_show_states(self, tv_show_states):
-        save_path = self._content['save_path']
+        save_path = Path(self._content['save_path'])
         username = self._content['username']
 
-        if not os.path.isdir(save_path):
+        if not save_path.is_dir():
             raise ValueError(f'save_path "{save_path}" does not exist')
 
         datetime_string = datetime.now().isoformat('_', 'seconds') \
@@ -38,7 +38,7 @@ class TvTimeExtractor(object):
             .replace(':', '')
         file_name = f'{username}_{datetime_string}.json'
 
-        file_path = os.path.join(save_path, file_name)
+        file_path = save_path.joinpath(file_name)
 
         logger.info(f'Saving data to "{file_path}"')
 
@@ -50,17 +50,17 @@ class TvTimeExtractor(object):
         argv = sys.argv
 
         if len(argv) >= 2:
-            if os.path.isabs(argv[1]):
+            if Path(argv[1]).is_absolute():
                 return argv[1]
             else:
-                return os.path.join(os.getcwd(), argv[1])
+                return Path.cwd().joinpath(argv[1])
         else:
-            return os.path.join(os.getcwd(), 'config.yaml')
+            return Path.cwd().joinpath('config.yaml')
 
     def _read_config(self):
         config_path = self._get_config_path()
 
-        if not os.path.exists(config_path):
+        if not config_path.exists():
             raise ValueError(f'Config path "{config_path}" does not exist')
 
         logger.info(f'Reading "{config_path}"')
@@ -75,13 +75,16 @@ class TvTimeExtractor(object):
                 raise ValueError('password is empty in config.yaml')
 
             if 'save_path' not in content or content['save_path'] is None:
-                content['save_path'] = os.getcwd()
-            else:
-                if not os.path.exists(content['save_path']):
-                    raise ValueError(f'Config path "{content["save_path"]}" does not exist')
+                content['save_path'] = Path.cwd()
+
+            content['save_path'] = Path(content['save_path'])
+
+            if not content['save_path'].exists():
+                raise ValueError(f'Config path "{content["save_path"]}" does not exist')
 
             log_content = dict(content)
             log_content['password'] = '*' * len(log_content['password'])
+            log_content['save_path'] = str(log_content['save_path'])
 
             logger.info(f'Config values: {log_content}')
 
