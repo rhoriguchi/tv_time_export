666a89f799abf92589620401c5189173a2a47750
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-07-17 17:30:46 +0200

    Upgrade dependency and unpin charset-normalizer

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     install_requires=[
         'PyYAML==6.0',
         'beautifulsoup4==4.11.1',
-        'charset-normalizer==1.3.3',
-        'requests==2.27.1',
+        'requests==2.28.1',
         'retrying==1.3.3'
     ],
     license='AGPL-3.0',
